621e072363cada86b5914c01d0c095a6148a4613
Jim Jeffers <shout@jimjeffers.com>
2012-01-22 12:28:48 -0700

    Added post navigation to blog.

--- Gemfile
+++ Gemfile
@@ -2,10 +2,4 @@ source 'http://rubygems.org'
 
 gem 'heroku'
 gem 'jekyll'
-gem 'rdiscount'
-gem 'gsl'
-
-group :development do
-  gem 'compass'
-  gem 'foreman'
-end
+gem 'jekyll-svg-plugin'
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,35 +1,8 @@
-$(function() {
-  // Open external links in a new tab/window
-  $('a').each(function() {
-     var a = new RegExp('/' + window.location.host + '/');
-     if(!a.test(this.href)) {
-         $(this).attr('rel', $.trim([$(this).attr('rel'), 'external'].join(' ')))
-
-         $(this).click(function(event) {
-             event.preventDefault(); event.stopPropagation();
-             window.open(this.href, '_blank');
-         });
-     }
-  });
-
-  // Landing page client list hover effect
-  $('#small-client-list ul li > a:not(.more), #endorsements ul li blockquote').hover(
-    function() {
-      $(this).animate({ opacity: 1 }, 400, 'swing');
-    },
-    function() {
-      $(this).animate({ opacity: 0.5 }, 400, 'swing');
+setTimeout(function(){
+  if(Modernizr.svg) {
+    var nav = document.getElementById("posts_nav")
+    if(nav != null) {
+      nav.className = "active"
     }
-  );
-
-  // Image attribution
-  $('img[data-attribution]').each(function() {
-    var attribution = $('<div class="attribution">').append($(this).attr('data-attribution'))
-
-    if($(this).attr('data-attribution-url')) {
-      attribution = $('<a rel="nofollow" href="' + $(this).attr('data-attribution-url') + '">').append(attribution);
-    }
-
-    $(this).after(attribution);
-  });
-});
+  }
+}, 500);
